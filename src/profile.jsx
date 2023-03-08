697ed0de6bfd1c4fba60cdc0206bdeb579412f57
uzairfarooq2005 <uzair.farooq99@gmail.com>
2023-03-08 23:03:51 +0500

    video player

--- src/profile.jsx
+++ src/profile.jsx
@@ -1,5 +1,53 @@
-import { View } from "react-native";
+import { Button, View, StyleSheet, Image, allowsEditing } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { Video } from "expo-av";
+
 function Profile() {
-  return <View style={{ flex: 1, backgroundColor: "pink" }}></View>;
+  const [imageUri, setImageUri] = useState("");
+  const [videoUri, setVideoUri] = useState("");
+
+  const openGallery = () => {
+    ImagePicker.getMediaLibraryPermissionsAsync().then((response) => {
+      console.log("permission Granted");
+    });
+
+    ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+    })
+      .then((response) => {
+        if (response.uri !== undefined) {
+          setImageUri(response.uri);
+          setVideoUri(response.uri);
+        }
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <View style={styles.container}>
+      {videoUri !== undefined ? (
+        <Video
+          style={{ width: "100%", height: "50%" }}
+          source={{ uri: videoUri }}
+          useNativeControls={true}
+        />
+      ) : (
+        <View />
+      )}
+
+      <Button title={"Open Gallery"} onPress={openGallery} />
+    </View>
+  );
 }
 export { Profile };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
